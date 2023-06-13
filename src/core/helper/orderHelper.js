8d1998d12e9c88de0fd1920d2c2ce9a7725a710f
SAHANI <sss921613@tatatechnologies.com>
2023-06-13 22:13:50 +0530

    order api calls

--- src/core/helper/orderHelper.js
+++ src/core/helper/orderHelper.js
@@ -1,5 +1,7 @@
 const API = process.env.REACT_APP_BACKEND;
 
+//TODO: CREATE ORDER
+
 export const createOrder = (userID, token, orderData) => {
   return fetch(`${API}/order/create/${userID}`, {
     method: "POST",
@@ -9,6 +11,40 @@ export const createOrder = (userID, token, orderData) => {
       Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ order: orderData }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//TODO: GET ALL ORDERS
+
+export const getAllOrders = (userID, token) => {
+  return fetch(`${API}/order/all/${userID}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//TODO: GET ORDER STATUS
+
+
+export const getOrderStatus = (userID, token) => {
+  return fetch(`${API}order/status/${userID}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   })
     .then((response) => {
       return response.json();
